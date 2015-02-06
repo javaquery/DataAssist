0c315b12c860f64afd94e5cd26e760df24115458
vicky.thakor <vicky.thakor@javaquery.com>
2015-02-06 11:22:51 +0530

    Project setup

---
+++ src/com/hibernateassist/HibernateAssist.java
@@ -0,0 +1,13 @@
+package com.hibernateassist;
+
+public class HibernateAssist {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
