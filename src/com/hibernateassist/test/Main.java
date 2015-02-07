470750f1aad6c82cc764b8c52118e70f1d453b85
vicky.thakor <vicky.thakor@javaquery.com>
2015-02-08 00:01:16 +0530

    Sampel Criteria

--- src/com/hibernateassist/test/Main.java
+++ src/com/hibernateassist/test/Main.java
@@ -1,5 +1,14 @@
 package com.hibernateassist.test;
 
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.hibernateassist.bean.User;
+
 public class Main {
 
 	/**
@@ -7,7 +16,28 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		try {
+			/* Create hibernate configuration. */
+			Configuration objConfiguration = new Configuration();
+			objConfiguration.configure("com\\hibernateassist\\hbm\\hibernate.cfg.xml");
+			
+			/* Open session and begin database transaction for database operation. */
+			SessionFactory objSessionFactory = objConfiguration.buildSessionFactory();
+	        Session objSession = objSessionFactory.openSession();
+	        
+	        Criteria criteria = objSession.createCriteria(User.class);
+	        List<User> listUser = criteria.list();
+	        
+	        for (User user : listUser) {
+				System.out.println(user.getUsername());
+			}
+	        
+	        objSession.close();
+	        objSessionFactory.close();
+	        
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 
 }
