acc504d157a9a4715e109818abcd003bef2ef409
vicky.thakor <vicky.thakor@javaquery.com>
2015-02-07 23:59:33 +0530

    Hibernate Configuration File. User, Message Bean and Hibernate Mapping File

---
+++ src/com/hibernateassist/bean/User.java
@@ -0,0 +1,36 @@
+package com.hibernateassist.bean;
+
+/**
+ * @author vicky.thakor
+ */
+public class User {
+	private int id;
+	private String Username;
+	private String Password;
+	private String Email;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return Username;
+	}
+	public void setUsername(String username) {
+		Username = username;
+	}
+	public String getPassword() {
+		return Password;
+	}
+	public void setPassword(String password) {
+		Password = password;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+}
