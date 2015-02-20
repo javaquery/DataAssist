2e0017ee8cbc2b7c8841e4b2ec08b764cb9f0ab3
vicky.thakor <vicky.thakor@javaquery.com>
2015-02-21 00:18:30 +0530

    Analyse .sqlplan from file

---
+++ src/com/hibernateassist/test/ReportFromFile.java
@@ -0,0 +1,21 @@
+package com.hibernateassist.test;
+
+import java.io.IOException;
+
+import com.hibernateassist.HibernateAssist;
+
+public class ReportFromFile {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		 HibernateAssist objHibernateAssist = new HibernateAssist();
+	        objHibernateAssist.setMSSQLExecutionPlanFile("C:\\Users\\0Signals\\Desktop\\HTMLReport\\ComplexPlan.sqlplan");
+	        objHibernateAssist.setHTMLReportFolder("C:\\Users\\0Signals\\Desktop\\HTMLReport");
+	        objHibernateAssist.analyseMSSQLPlan();
+
+	}
+
+}
