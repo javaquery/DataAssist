8a0f312c8fd9ada6469b3c5bbea488059faa19e7
vicky.thakor <vicky.thakor@javaquery.com>
2015-03-07 15:44:20 +0530

    Message Bean

--- src/com/hibernateassist/bean/Message.java
+++ src/com/hibernateassist/bean/Message.java
@@ -2,5 +2,32 @@ package com.hibernateassist.bean;
 
 public class Message {
 	private int id;
+	private int userID;
 	private int messageText;
+	private String Username;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getUserID() {
+		return userID;
+	}
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+	public int getMessageText() {
+		return messageText;
+	}
+	public void setMessageText(int messageText) {
+		this.messageText = messageText;
+	}
+	public String getUsername() {
+		return Username;
+	}
+	public void setUsername(String username) {
+		Username = username;
+	}
 }
