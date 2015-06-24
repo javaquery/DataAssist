25d3b45a12e6653e0e10bf509b0c15de1bc52e5b
vicky.thakor <vicky.thakor@javaquery.com>
2015-06-24 23:23:19 +0530

    Common Files

--- src/com/hibernateassist/test/Main.java
+++ src/com/hibernateassist/test/Main.java
@@ -3,12 +3,10 @@ package com.hibernateassist.test;
 import java.util.List;
 
 import org.hibernate.Criteria;
-import org.hibernate.FetchMode;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.criterion.Conjunction;
-import org.hibernate.criterion.Criterion;
 import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
@@ -33,7 +31,7 @@ public class Main {
 	        Session objSession = objSessionFactory.openSession();
 	        
 	        Criteria criteria = objSession.createCriteria(User.class);
-	        /*criteria.setProjection(Projections.projectionList().add(Projections.max("id")));*/
+	        /*criteria.setProjection(Projections.projectionList().add(Projections.groupProperty("id")));*/
 	        criteria.createAlias("Messages", "Messages");
 	        criteria.createAlias("CreditCard", "CreditCard");
 	        
@@ -41,24 +39,26 @@ public class Main {
 	        /* Create object of Conjunction */
 	        Conjunction objConjunction = Restrictions.conjunction();
 	        /* Add multiple condition separated by AND clause. */
-	        /*objConjunction.add(Restrictions.eq("Messages.userID", 1));
-	        objConjunction.add(Restrictions.eq("Messages.userID", 2));*/
+	        objConjunction.add(Restrictions.eq("Messages.userID", 1));
+	        objConjunction.add(Restrictions.eq("Messages.userID", 2));
 	        
 	        /* Attach Conjunction in Criteria */
 	        /*criteria.add(objConjunction);*/
 	        
             /*criteria.add(Restrictions.eq("Email", "vicky.thakor@javaquery.com"));
-            criteria.addOrder(Order.asc("Username"));
-            criteria.setMaxResults(10);*/
+            criteria.addOrder(Order.asc("Username"));*/
+            criteria.setMaxResults(10);
             criteria.add(Restrictions.between("id", 1, 2));
 	        List<User> listUser = criteria.list();
 	        
 	        HibernateAssist objHibernateAssist = new HibernateAssist(objSession);
 	        objHibernateAssist.setCriteria(criteria);
-	        String strQuery = objHibernateAssist.getValuedCriteriaQuery();
-	        System.out.println(strQuery);
-	        objHibernateAssist.setHTMLReportFolder("C:\\Users\\0Signals\\Desktop\\Hibernate Assist\\PostgreSQL");
+	        objHibernateAssist.setHTMLReportFolder("C:\\Users\\0Signals\\Desktop\\Hibernate Assist\\Oracle", "Limit_Between");
 	        objHibernateAssist.analyseCriteria();
+	        
+	        
+	        /*String strQuery = objHibernateAssist.getValuedCriteriaQuery();
+	        System.out.println(strQuery);*/
 	        
 	        /*for (User user : listUser) {
 				System.out.println(user.getUsername());
